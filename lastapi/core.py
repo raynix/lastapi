1a28021f08b4d490feaa2e9090a83ec157ea8fc9
Raymond Xu <ray.xu@contino.io>
2019-07-15 10:26:47 +1000

    Variable pattern now as a variable.

--- lastapi/core.py
+++ lastapi/core.py
@@ -3,27 +3,60 @@ import requests
 import re
 
 from types import SimpleNamespace
+from pykwalify.core import Core
 
-PARAMETER_PATTERN = re.compile('\$\{([^\}]+)\}')
-def traverse_schema(schema, params):
+GLOBAL_PARAMETER_PATTERN = re.compile('(\$\{([^\}]+)\})')
+ARGUMENTS_PATTERN = re.compile('(\#\{([^\}]+)\})')
+# schema is a dict object reading from yaml
+def traverse_schema(schema, pattern, params={}):
   if isinstance(schema, dict):
     processed_schema = {}
     for key, value in schema.items():
-      processed_schema[key] = traverse_schema(value, params)
+      processed_schema[key] = traverse_schema(value, pattern, params)
     return SimpleNamespace(**processed_schema)
 
   elif isinstance(schema, list):
-    return [ traverse_schema(v, params) for v in schema ]
+    return [ traverse_schema(v, pattern, params) for v in schema ]
+  elif isinstance(schema, str):
+    return process_parameters(schema, pattern, params)
   else:
-    return process_parameters(schema, params)
+    return schema
 
-def process_parameters(template, params):
-  if re.search(PARAMETER_PATTERN, template):
-    return template.replace('${', '{').format(**params)
+def process_parameters(template, compiled_pattern, params):
+  found = compiled_pattern.search(template)
+  if found:
+    return replace_argument(template, found).format(**params)
   else:
     return template
 
+def replace_argument(template, match):
+  groups = match.groups()
+  search = groups[0]
+  if len(groups) > 1:
+    replace = groups[1]
+  else:
+    replace = groups[0]
+  return template.replace(search, "{{{0}}}".format(replace))
+
 class LastApi:
   def __init__(self, api_name):
-    with open(api_name, 'r') as fp:
-      self.schema = traverse_schema(yaml.load(fp), {})
+    api_definition_file = "{0}.yaml".format(api_name)
+    api_parameters_file = "{0}-params.yaml".format(api_name)
+    c = Core(source_file=api_definition_file, schema_files=["schemas/schema.yaml"])
+    c.validate(raise_exception=True)
+    with open(api_parameters_file, 'r') as fp:
+      params = yaml.load(fp)
+    with open(api_definition_file, 'r') as fp:
+      self.schema = yaml.load(fp)
+    self.headers = traverse_schema(self.schema['Headers'], GLOBAL_PARAMETER_PATTERN, params)
+
+  def invoke(self, action_name, params):
+    pass
+
+"""
+  curl -X PUT $CFAPI/zones/$1/dns_records/$2 \
+    -H "X-Auth-Key: $CFKEY" \
+    -H "X-Auth-Email: raynix@gmail.com" \
+    -H "Content-type: application/json" \
+    --data "{\"type\":\"$3\",\"name\":\"$4\",\"content\":\"$5\",\"ttl\":1,\"proxied\":true}"
+"""
--- tests/test_core.py
+++ tests/test_core.py
@@ -5,7 +5,7 @@ class TestApiCore(unittest.TestCase):
   def test_process_parameters(self):
     test_template = 'test${variable}'
     test_params = { 'variable': 'asdf' }
-    self.assertEqual(process_parameters(test_template, test_params), 'testasdf')
+    self.assertEqual(process_parameters(test_template,GLOBAL_PARAMETER_PATTERN, test_params), 'testasdf')
 
   def test_traverse_schema(self):
     test_schema = {
@@ -19,10 +19,10 @@ class TestApiCore(unittest.TestCase):
       'key2': { 'param1': 'testasdf'},
       'key3': [ 'alpha', 'beta', 'theta' ]
     }
-    sn = traverse_schema(test_schema, test_params)
+    sn = traverse_schema(test_schema, GLOBAL_PARAMETER_PATTERN, test_params)
     self.assertEqual(sn.key1, assert_schema['key1'])
-    self.assertEqual(sn.key2.param1, 'testasdf')
-    self.assertEqual(sn.key3[2], 'theta')
+    self.assertEqual(sn.key2.param1, assert_schema['key2']['param1'])
+    self.assertEqual(sn.key3[2], assert_schema['key3'][2])
 
 if __name__ == '__main__':
   unittest.main()
